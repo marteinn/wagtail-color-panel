4b2a039295ab9aba2fba5f1680483e8923af5239
Martin Sandström <martin@marteinn.se>
2020-07-31 11:04:39 +0200

    Prepare files for release

--- setup.py
+++ setup.py
@@ -1,22 +1,38 @@
+import io
+import re
+import sys
+import os
 from os import path
 from setuptools import setup, find_packages
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
 
 
 install_requires = ["wagtail"]
 
 tests_require = ["pytest-django", "wagtail-factories", "pytest"]
 
-with open(
-    path.join(path.abspath(path.dirname(__file__)), "README.md"), encoding="utf-8"
-) as f:
-    long_description = f.read()
+# Convert markdown to rst
+try:
+    from pypandoc import convert
+    long_description = convert("README.md", "rst")
+except:  # NOQA
+    long_description = ""
+
+version = ''
+with io.open('wagtail_color_panel/__init__.py', 'r', encoding='utf8') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
 
 setup(
-    version="1.0.0",
+    version=version,
     name="wagtail-color-panel",
     description="",
     long_description=long_description,
-    long_description_content_type="text/markdown",
     author="Martin Sandström",
     author_email="martin@marteinn.se",
     url="https://github.com/marteinn/wagtail-color-panel",
@@ -31,7 +47,7 @@ setup(
     license="MIT",
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Environment :: Web Environmen",
+        "Environment :: Web Environment",
         "Framework :: Django",
         "Framework :: Wagtail",
         "Framework :: Wagtail :: 2",
---
+++ wagtail_color_panel/__init__.py
@@ -0,0 +1,6 @@
+__title__ = "wagtail_color_panel"
+__version__ = "1.0.1"
+__build__ = 2
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020-present Martin Sandström"
