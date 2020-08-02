84bccbe6c25b19798edd7e2fbe5555cc041c4722
Martin Sandström <martin@marteinn.se>
2020-08-02 10:43:58 +0200

    Bump version

--- wagtail_color_panel/__init__.py
+++ wagtail_color_panel/__init__.py
@@ -1,6 +1,6 @@
 __title__ = "wagtail_color_panel"
-__version__ = "1.0.1"
-__build__ = 2
+__version__ = "1.1.0"
+__build__ = 3
 __author__ = "Martin Sandström"
 __license__ = "MIT"
 __copyright__ = "Copyright 2020-present Martin Sandström"
