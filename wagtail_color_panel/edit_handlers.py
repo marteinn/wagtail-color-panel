af6e86806214af00f679d37c99d3aa7fb9787207
Brian Xu <brian.xu@torchbox.com>
2022-06-23 15:47:16 +0800

    fix NativeColorPanel

--- wagtail_color_panel/edit_handlers.py
+++ wagtail_color_panel/edit_handlers.py
@@ -10,9 +10,17 @@ else:
 
 class NativeColorPanel(FieldPanel):
     def widget_overrides(self):
+        # For Wagtail<3.0 we use widget_overrides
         return {
             self.field_name: ColorInputWidget(),
         }
+
+    def get_form_options(self):
+        # For Wagtail 3.0 we use get_form_options
+        # So we can mix them to provide supports to Wagtail 2,3
+        opts = super().get_form_options()
+        opts["widgets"] = self.widget_overrides()
+        return opts
 
 
 class PolyfillColorPanel(FieldPanel):
