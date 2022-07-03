f7022659663d461a78c3156b96d819d21d89f825
Martin Sandström <martin@marteinn.se>
2022-07-03 08:18:20 +0200

    Revert "Wagtail 3.0 support"

--- tests/urls.py
+++ tests/urls.py
@@ -1,13 +1,8 @@
-from django.urls import include, path
-from wagtail import VERSION as WAGTAIL_VERSION
+from django.conf.urls import include, url
 from wagtail.admin import urls as wagtailadmin_urls
-
-if WAGTAIL_VERSION >= (3, 0):
-    from wagtail import urls as wagtail_urls
-else:
-    from wagtail.core import urls as wagtail_urls
+from wagtail.core import urls as wagtail_urls
 
 urlpatterns = [
-    path("admin/", include(wagtailadmin_urls)),
-    path("", include(wagtail_urls)),
+    url(r"^admin/", include(wagtailadmin_urls)),
+    url(r"", include(wagtail_urls)),
 ]
